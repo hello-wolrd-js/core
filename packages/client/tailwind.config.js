afb62116c28d249db9d359964936a10fb9f73a76
Lin <2295141009@stu.hqu.edu.cn>
2024-04-13 16:05:28 +0800

    style: 增加两种像素字体

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            fontFamily: {
+                'press-start': ['"PressStart2P"'],
+                zpix: ['Zpix']
+            }
+        }
     },
     plugins: [require('daisyui')],
     daisyui: {
