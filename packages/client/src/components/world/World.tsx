38f57798b52cd7d8ab66b7af21ca041f25579ae7
Lin <2295141009@stu.hqu.edu.cn>
2024-04-15 21:42:30 +0800

    style: iframe默认占满

--- packages/client/src/components/world/World.tsx
+++ packages/client/src/components/world/World.tsx
@@ -4,7 +4,7 @@ const World: Component = () => {
     return (
         <iframe
             id="world-container"
-            style={{ width: '100%', height: '750px' }}
+            style={{ width: '100%', height: `${document.body.clientHeight - 60}px` }}
             sandbox="allow-same-origin allow-scripts"
         />
     )
