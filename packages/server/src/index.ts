2fbe3deaa443abae6772d85552547c75d1f69613
Lin <2295141009@stu.hqu.edu.cn>
2024-04-15 21:34:39 +0800

    feat: 后端加一点测试数据

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,27 +1,9 @@
 import { Elysia } from 'elysia'
 import { cors } from '@elysiajs/cors'
-import type { WorldCard } from '@core/models'
-
+import cards from '../__tests__/card/01'
 new Elysia()
     .use(cors())
-    .get('/cards', () => {
-        const cards: WorldCard[] = Array.from({ length: 10 }, () => {
-            return {
-                title: 'Kancy Joe',
-                cover: 'https://pub-a08fa93d49d347298f3cfbf1f32118b8.r2.dev/Q-%E5%A4%A7%E5%8F%94.png',
-                content: 'jooooooe!',
-                // style: {
-                //     width: 200,
-                //     height: 400
-                // },
-                url: `./src/__tests__/cards/your-world.mjs`,
-                total: {
-                    star: 111
-                }
-            }
-        })
-        return cards
-    })
+    .get('/cards', () => cards)
     .listen(4000)
 
 console.log('server listen on 4000')
