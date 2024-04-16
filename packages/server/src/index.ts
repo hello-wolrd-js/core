e3e08c6534b2a52cd86f2b6b6a28c9d49a2dcc50
Lin <2295141009@stu.hqu.edu.cn>
2024-04-16 16:10:31 +0800

    build: elysia static plugin

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,10 @@
 import { Elysia } from 'elysia'
 import { cors } from '@elysiajs/cors'
+import { staticPlugin } from '@elysiajs/static'
 import cards from '../__tests__/card/01'
 new Elysia()
     .use(cors())
+    .use(staticPlugin())
     .get('/cards', () => cards)
     .listen(4000)
 
