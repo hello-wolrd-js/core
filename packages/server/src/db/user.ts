45c406e30bd263fb1e8f27209415bc664351a9d3
Tyee <flin00806@gmail.com>
2024-04-22 10:36:11 +0800

    fix(server): 修复注册用户时意外返回_id字段

--- packages/server/src/db/user.ts
+++ packages/server/src/db/user.ts
@@ -12,7 +12,7 @@ export const createUser = async ({ username, password }: LoginParams) => {
     const user = await UserModel.findOne({ username })
     if (user) throw '存在同名用户!'
     //通过接口创建的用户只能是common, 管理员必须手动改
-    return await UserModel.create({ username, password, role: 'common' })
+    return (await UserModel.create({ username, password, role: 'common' })).getInfo()
 }
 
 export const getUser = async (id: string) => {
